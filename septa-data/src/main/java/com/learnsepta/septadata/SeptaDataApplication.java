a70a8ca7e43a552a8a53bd2c4e7a70529669dc55
jimmychung98 <joc98@pitt.edu>
2018-10-31 21:53:26 -0400

    Finished calculating the Total Distance in the KML files

--- septa-data/src/main/java/com/learnsepta/septadata/SeptaDataApplication.java
+++ septa-data/src/main/java/com/learnsepta/septadata/SeptaDataApplication.java
@@ -12,8 +12,12 @@ public class SeptaDataApplication {
     public static void main(String args[]) {
         SpringApplication.run(SeptaDataApplication.class);
 
-
-        KMLParser kml = new KMLParser("busRoutes/17.kml");
+        // http://www3.septa.org/transitview/kml/17.kml
+        //KMLParser kml = new KMLParser("busRoutes/17.kml");
+        //KMLParser kml = new KMLParser("http://www3.septa.org/transitview/kml/17.kml");
+        for (int index = 1; index < 10; index++) {
+            KMLParser kml = new KMLParser("http://www3.septa.org/transitview/kml/" + index + ".kml");
+        }
     }
     
 }
--- septa-data/src/main/java/com/learnsepta/septadata/helper/KMLParser.java
+++ septa-data/src/main/java/com/learnsepta/septadata/helper/KMLParser.java
@@ -1,18 +1,105 @@
 package com.learnsepta.septadata.helper;
 
-import java.io.*;
-
-import org.springframework.core.io.*;
+import java.util.ArrayList;
+import java.net.URL;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 public class KMLParser {
-    public KMLParser(String file_name) {
-        try {
-            Resource resource = new ClassPathResource(file_name);
-            InputStream resourceInputStream = resource.getInputStream();
+    private Document doc;
+    private ArrayList<String> coordinates;
+    private double totalDistance;
+
+    public KMLParser(String kml_website) {
+        try {           
+            System.out.println("----------------------------");
+            URL url = new URL(kml_website);
+            System.out.println("Initializing Coordinates for: " + url.toString());
+            //Resource resource = new ClassPathResource(file_name);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder  dBuilder = dbFactory.newDocumentBuilder();
+            //doc = dBuilder.parse(resource.getFile());
+            doc = dBuilder.parse(url.openStream());
+            doc.getDocumentElement().normalize();
+
+            totalDistance = 0;
+
+            initCoordinates();
+            calculateTotalDistance();
             
+            System.out.println("----------------------------");
         } catch (Exception ex) {
             System.out.println(ex);
         }
-        
+    }
+
+    private void initCoordinates() {
+        NodeList lineStrings = doc.getElementsByTagName("LineString");
+        Node lineString;
+        coordinates = new ArrayList<String>();
+
+        for (int index = 0; index < lineStrings.getLength(); index++) {
+            lineString = lineStrings.item(index);
+
+            if (lineString.getNodeType() == Node.ELEMENT_NODE) {
+                Element coordWrapper = (Element) lineString;
+                Node coord = coordWrapper.getElementsByTagName("coordinates").item(0).getLastChild();
+                coord.normalize();
+
+                coordinates.add(coord.getNodeValue());
+            }
+        }
+    }
+
+    private void calculateTotalDistance() {
+        String[] coords_set;
+        String[] coords_one_str;
+        double[] coords_one = new double[3];
+        String[] coords_two_str;
+        double[] coords_two = new double[3];
+        for (int x = 0; x < coordinates.size(); x++) {
+            coords_set = coordinates.get(x).split("\\s+");
+            for (int y = 1; y < coords_set.length - 1; y++) {
+                coords_one_str = coords_set[y].split(",");
+                coords_two_str= coords_set[y+1].split(",");
+                for (int index = 0; index < coords_one_str.length; index++) {
+                    coords_one[index] = Double.parseDouble(coords_one_str[index]);
+                    coords_two[index] = Double.parseDouble(coords_two_str[index]);
+                }
+
+                distance(coords_one[1], coords_two[1], coords_one[0], coords_two[0], coords_one[2], coords_two[2]);
+            }
+        }
+
+        System.out.println("Total Distance: " + this.totalDistance + " miles");
+    }
+
+    private final int RADIUS = 6371;
+
+    public void distance(double lat1, double lat2, double lng1,
+        double lng2, double el1, double el2) {
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        // Convert to Miles
+        double distance = RADIUS * c * 1000 * 0.000621371; 
+
+        double height = el1 - el2;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        totalDistance += Math.sqrt(distance);
+    }
+
+    public double returnTotalDistance() {
+        return totalDistance;
     }
 }
